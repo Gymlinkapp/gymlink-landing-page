f606933ee5ebb56ab434f8e43a5d3c9cc7ed4901
Dylan <dylan.reeder5@gmail.com>
2023-05-08 10:21:57 -0400

    add partytown to handle google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });

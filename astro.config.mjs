4785405962de231be76e980c1f39107ee0ec8591
Dylan <dylan.reeder5@gmail.com>
2023-05-08 10:24:27 -0400

    save astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     partytown({
-
       // Adds dataLayer.push as a forwarding-event.
       config: {
         forward: ["dataLayer.push"],
       },
     }),
-  ]
+  ],
 });
